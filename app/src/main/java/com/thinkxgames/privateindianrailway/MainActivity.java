8c0b20b3f74ffab3ace2990dba96376efb1f665c
saikatdebnath <saikat.dce@gmail.com>
2017-02-08 17:59:11 +0530

    AppEngine end-to-end

--- app/src/main/java/com/thinkxgames/privateindianrailway/MainActivity.java
+++ app/src/main/java/com/thinkxgames/privateindianrailway/MainActivity.java
@@ -1,106 +1,14 @@
 package com.thinkxgames.privateindianrailway;
 
-import android.content.Context;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Pair;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.Map;
+import com.loopj.android.http.*;
 
-import javax.net.ssl.HttpsURLConnection;
-
-class ServletPostAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
-    private Context context;
-
-    @Override
-    protected String doInBackground(Pair<Context, String>... params) {
-        context = params[0].first;
-        String name = params[0].second;
-
-        try {
-            // Set up the request
-            URL url = new URL("http://10.0.2.2:8080/name");
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("POST");
-            connection.setDoInput(true);
-            connection.setDoOutput(true);
-
-            // Build name data request params
-            Map<String, String> nameValuePairs = new HashMap<>();
-            nameValuePairs.put("name", name);
-            String postParams = buildPostDataString(nameValuePairs);
-
-            // Execute HTTP Post
-            OutputStream outputStream = connection.getOutputStream();
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
-            writer.write(postParams);
-            writer.flush();
-            writer.close();
-            outputStream.close();
-            connection.connect();
-
-            // Read response
-            int responseCode = connection.getResponseCode();
-            StringBuilder response = new StringBuilder();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                response.append(line);
-            }
-            reader.close();
-
-            if (responseCode == HttpsURLConnection.HTTP_OK) {
-                return response.toString();
-            }
-            return "Error: " + responseCode + " " + connection.getResponseMessage();
-
-        } catch (IOException e) {
-            return e.getMessage();
-        }
-    }
-
-    private String buildPostDataString(Map<String, String> params) throws UnsupportedEncodingException {
-        StringBuilder result = new StringBuilder();
-        boolean first = true;
-        for (Map.Entry<String, String> entry : params.entrySet()) {
-            if (first) {
-                first = false;
-            } else {
-                result.append("&");
-            }
-
-            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
-            result.append("=");
-            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
-        }
-
-        return result.toString();
-    }
-
-    @Override
-    protected void onPostExecute(String result) {
-        Toast.makeText(context, result, Toast.LENGTH_LONG).show();
-    }
-}
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -111,16 +19,39 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.post(this, "http://10.0.2.2:8080/hello", new RequestParams("name", "kchitra"), new AsyncHttpResponseHandler() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                try {
+                    System.out.println("Success, response : " + new String(responseBody, "UTF-8"));
+                } catch (Exception e) {
+
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                System.out.println("Chutiya kat gaya" + responseBody.toString() + "   " + error.toString());
             }
         });
+        String resultString = "";
+        try {
+            //resultString = results.get();
+        } catch (Exception e) {
 
-        new ServletPostAsyncTask().execute(new Pair<Context, String>(this, "Saikat"));
+        }
+
+        System.out.println("Results = " + resultString);
     }
 
     @Override
